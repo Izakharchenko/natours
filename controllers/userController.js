ce7eae5ba3d184871b9764bee73e80a853593bd2
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2024-02-04 13:30:27 +0200

    add(factory functions) createOne updateOne

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,11 +73,6 @@ exports.getUser = (req, res) => {
   });
 };
 
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This rout has not implemented yet.',
-  });
-};
-
+// Do not update password with this!
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
